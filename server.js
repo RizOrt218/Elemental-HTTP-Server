70512052c6cd449f8d9a9ed908ddb2916fbb3eaf
RizOrt218 <rizhiafalen@gmail.com>
2016-01-06 19:44:43 -1000

    GET method returns files header and body

--- server.js
+++ server.js
@@ -2,9 +2,34 @@ var http = require( 'http' );
 var fs = require( 'fs' );
 var url = require('url');
 
-var server = http.createServer( serversEventListener );
 
+var server = http.createServer( function ( req, socket, head ) {
+  // req.setEncoding( 'utf-8' );
+  var uri = req.url;
 
+  // console.log( req );
+  // console.log( req.headers );
+  // console.log( req.url );
+
+    socket.writeHead( 200, {});
+
+  if ( req.method === 'POST' ) {
+    console.log( "post detected" );
+  }
+
+  if ( req.method === 'GET' ) {
+    return fs.readFile( './public' + uri, function ( err , data ) {
+      if ( err ) {
+          return fs.readFile( './public/404.html' , function ( err , data ) {
+            socket.write( data );
+            socket.end( );
+          });
+      }
+      socket.write( data );
+      socket.end( );
+    }); // end of fs.readFile
+  } // GET method
+}); //end of server createServer( )
 
 // grab a random port.
 server.listen( { port: 8080 }, function() {
