45ef7170b5cd7ce0fede62a44fbacfbed9beb6db
RizOrt218 <rizhiafalen@gmail.com>
2016-01-06 21:05:22 -1000

    reads POST and saving a new file

--- server.js
+++ server.js
@@ -8,25 +8,37 @@ var server = http.createServer( function ( req, socket, head ) {
   var uri = req.url;
 
   // console.log( req );
-  // console.log( req.headers );
+  console.log( req.headers );
   // console.log( req.url );
-
-    socket.writeHead( 200, {});
 
   if ( req.method === 'POST' ) {
     console.log( "post detected" );
+    if ( uri === '/elements.html' ) {
+      return fs.readFile( './public/helium.html' , function ( err , data ) {
+        return fs.writeFile( './public' + uri , data );
+      });
+    }
   }
 
   if ( req.method === 'GET' ) {
+    if ( uri === '/' ) {
+      uri = '/index.html';
+    }
     return fs.readFile( './public' + uri, function ( err , data ) {
       if ( err ) {
-          return fs.readFile( './public/404.html' , function ( err , data ) {
-            socket.write( data );
-            socket.end( );
+        return fs.readFile( './public/404.html' , function ( err , data ) {
+          socket.writeHead( 404, {
+            'Server' : 'Rizzi-lush',
+            'Content-length' : data.length
           });
+          socket.end( data );
+        });
       }
-      socket.write( data );
-      socket.end( );
+      socket.writeHead( 200, {
+        'Server' : 'Rizzi-lush',
+        'Content-length' : data.length
+      });
+      socket.end( data );
     }); // end of fs.readFile
   } // GET method
 }); //end of server createServer( )
