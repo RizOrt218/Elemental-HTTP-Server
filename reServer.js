f874e4a1b24a586af8a67eddaa3f447210d58934
RizOrt218 <rizhiafalen@gmail.com>
2016-01-08 20:00:41 -1000

    requires files

--- reServer.js
+++ reServer.js
@@ -4,10 +4,12 @@ const http          = require( 'http' );
 const fs            = require( 'fs' );
 const url           = require( 'url' );
 const querystring   = require( 'querystring' );
+const getFile       = require( './method/GET' );
+const postFile       = require( './method/POST' );
+const putFile       = require( './method/PUT' );
+const deleteFile       = require( './method/DELETE' );
 
-var server = http.createServer( connectServer );
-
-function connectServer ( request, respond ) => {
+var server = http.createServer( ( request, respond ) => {
   request.setEncoding( 'utf-8' );
 
   let body = null;
@@ -17,11 +19,21 @@ function connectServer ( request, respond ) => {
     console.log( body );
   });
 
+  switch (request.method) {
+    case 'GET':
+      return getFile.GET(req, res);
+    case 'POST':
+      return req.url !== '/elements' ? postFile.ERROR(null, req, res) : postFile.POST(req, res, postFile.ERROR);
+    case 'PUT':
+      return putFile.PUT(req, res);
+    case 'DELETE':
+      return deleteFile.DELETE(req, res);
+    default:
+      return methodFile.ERROR(req, res);
+  }
 
+});
 
-}
-
-server.listen( { port: 2180 }, function() {
-  address = server.address();
-  console.log( "opened server on %j", address );
+server.listen( { port: 8212 }, function() {
+  console.log( 'server started http://localhost:8212' );
 });
--- server.js
+++ server.js
@@ -1,109 +1,109 @@
-var http = require( 'http' );
-var fs = require( 'fs' );
-var url = require( 'url' );
-var querystring = require( 'querystring' );
+// var http = require( 'http' );
+// var fs = require( 'fs' );
+// var url = require( 'url' );
+// var querystring = require( 'querystring' );
 
-var elemCount = 2;
-
-
-var server = http.createServer( function ( req, socket, head ) {
-  var newFileName;
-  var uri = req.url;
-
-  if ( req.method === 'POST' ) {
-
-    req.on( 'data', function( buffer ) {
-      var dataBuffer = querystring.parse( buffer.toString() );
-
-      if ( uri === '/elements' ) {
-        return fs.readFile( 'elementalTemplate.html' , function ( err , template ) {
-          newFileName = dataBuffer.elementName;
-
-          var tempString = template.toString()
-            .replace( /{{elementName}}/g, dataBuffer.elementName )
-            .replace( '{{elementSymbol}}', dataBuffer.elementSymbol )
-            .replace( '{{elementAtomicNumber}}', dataBuffer.elementAtomicNumber )
-            .replace( '{{elementDescription}}', dataBuffer.elementDescription );
-
-          return fs.writeFile( './public/' + newFileName  + '.html', tempString , function( err, tempString ) {
-
-            socket.writeHead( 200, {
-              'Server' : 'Rizzi-lush',
-            });
-            socket.end( tempString );
-
-            return fs.readFile( 'indexTemplate.html', function ( err, contents ) {
-              var pageLink = '<li><a href="/{{elementName}}.html">{{elementName}}</a></li>';
-              elemCount += 1;
-
-              var appendNewElem = contents.toString()
-                .replace( '<!--  {{ element list }} -->', pageLink + '<!--  {{ element list }} -->' )
-                .replace( /{{elementName}}/g, dataBuffer.elementName )
-                .replace( /\d+<!-- number -->/, elemCount + '<!-- number -->');
-
-              return fs.writeFile( './public/index.html', appendNewElem , function( err ) {
-                fs.writeFile( 'indexTemplate.html', appendNewElem , function( err ) {
-
-                  console.log(tempString);
-                  // addElemInArr();
-
-                });
-                socket.writeHead( 200, {
-                  'Server' : 'Rizzi-lush',
-                 });
-                socket.end( );
-              });
-            }); // end of return fs.readFile( 'index..
-          }); // end fs.writeFile
-        }); //end of fs.readFile
-      } // end of if ( uri === '/elements' )
-    }); // end of req.on( 'data', function( buffer )
-  } // end of if ( req.metho ..
-
-  if ( req.method === 'GET' ) {
-    if ( uri === '/' ) {
-      uri = '/index.html';
-    }
-    return fs.readFile( './public' + uri, function ( err , data ) {
-      if ( err ) {
-        return fs.readFile( './public/404.html' , function ( err , data ) {
-          socket.writeHead( 404, {
-            'Server' : 'Rizzi-lush',
-            'Content-length' : data.length,
-            'Message' : 'Please write a valid path'
-          });
-          socket.end( data );
-        });
-      }
-      socket.writeHead( 200, {
-        'Server' : 'Rizzi-lush',
-        'Content-length' : data.length
-      });
-      socket.end( data );
-    }); // end of fs.readFile
-  } // GET method
+// var elemCount = 2;
 
 
-// function addElemInArr( ) {
-//   var found = false;
+// var server = http.createServer( function ( req, socket, head ) {
+//   var newFileName;
+//   var uri = req.url;
 
-//   for ( var i = 0; i < elementArr.length; i++ ) {
-//     elementArr.push( newFileName );
-//     found = true;
+//   if ( req.method === 'POST' ) {
 
-//     // elemCount += 1;
-//   console.log( newFileName );
-//   console.log( elementArr );
-//   console.log( elemCount );
-//     return found;
-//   }
-// } //end of addEleminArr
+//     req.on( 'data', function( buffer ) {
+//       var dataBuffer = querystring.parse( buffer.toString() );
 
-}); // end of server createServer( )
+//       if ( uri === '/elements' ) {
+//         return fs.readFile( 'elementalTemplate.html' , function ( err , template ) {
+//           newFileName = dataBuffer.elementName;
+
+//           var tempString = template.toString()
+//             .replace( /{{elementName}}/g, dataBuffer.elementName )
+//             .replace( '{{elementSymbol}}', dataBuffer.elementSymbol )
+//             .replace( '{{elementAtomicNumber}}', dataBuffer.elementAtomicNumber )
+//             .replace( '{{elementDescription}}', dataBuffer.elementDescription );
+
+//           return fs.writeFile( './public/' + newFileName  + '.html', tempString , function( err, tempString ) {
+
+//             socket.writeHead( 200, {
+//               'Server' : 'Rizzi-lush',
+//             });
+//             socket.end( tempString );
+
+//             return fs.readFile( 'indexTemplate.html', function ( err, contents ) {
+//               var pageLink = '<li><a href="/{{elementName}}.html">{{elementName}}</a></li>';
+//               elemCount += 1;
+
+//               var appendNewElem = contents.toString()
+//                 .replace( '<!--  {{ element list }} -->', pageLink + '<!--  {{ element list }} -->' )
+//                 .replace( /{{elementName}}/g, dataBuffer.elementName )
+//                 .replace( /\d+<!-- number -->/, elemCount + '<!-- number -->');
+
+//               return fs.writeFile( './public/index.html', appendNewElem , function( err ) {
+//                 fs.writeFile( 'indexTemplate.html', appendNewElem , function( err ) {
+
+//                   console.log(tempString);
+//                   // addElemInArr();
+
+//                 });
+//                 socket.writeHead( 200, {
+//                   'Server' : 'Rizzi-lush',
+//                  });
+//                 socket.end( );
+//               });
+//             }); // end of return fs.readFile( 'index..
+//           }); // end fs.writeFile
+//         }); //end of fs.readFile
+//       } // end of if ( uri === '/elements' )
+//     }); // end of req.on( 'data', function( buffer )
+//   } // end of if ( req.metho ..
+
+//   if ( req.method === 'GET' ) {
+//     if ( uri === '/' ) {
+//       uri = '/index.html';
+//     }
+//     return fs.readFile( './public' + uri, function ( err , data ) {
+//       if ( err ) {
+//         return fs.readFile( './public/404.html' , function ( err , data ) {
+//           socket.writeHead( 404, {
+//             'Server' : 'Rizzi-lush',
+//             'Content-length' : data.length,
+//             'Message' : 'Please write a valid path'
+//           });
+//           socket.end( data );
+//         });
+//       }
+//       socket.writeHead( 200, {
+//         'Server' : 'Rizzi-lush',
+//         'Content-length' : data.length
+//       });
+//       socket.end( data );
+//     }); // end of fs.readFile
+//   } // GET method
 
 
-// grab a random port.
-server.listen( { port: 8080 }, function() {
-  address = server.address();
-  console.log( "opened server on %j", address );
-});
+// // function addElemInArr( ) {
+// //   var found = false;
+
+// //   for ( var i = 0; i < elementArr.length; i++ ) {
+// //     elementArr.push( newFileName );
+// //     found = true;
+
+// //     // elemCount += 1;
+// //   console.log( newFileName );
+// //   console.log( elementArr );
+// //   console.log( elemCount );
+// //     return found;
+// //   }
+// // } //end of addEleminArr
+
+// }); // end of server createServer( )
+
+
+// // grab a random port.
+// server.listen( { port: 8080 }, function() {
+//   address = server.address();
+//   console.log( "opened server on %j", address );
+// });
