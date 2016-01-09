451670dd1e1235aa8c4c7200229e27216c5433c8
RizOrt218 <rizhiafalen@gmail.com>
2016-01-08 23:58:10 -1000

    adds commmeeents

--- method/GET.js
+++ method/GET.js
@@ -10,11 +10,13 @@ module.exports = (function() {
     console.log( 'get request detected!')
     let uri = request.url;
 
+    //redirects client if path is / or empty string
     if ( uri === '/' || '') {
       uri = '/index.html';
     }
     return fs.readFile( './public' + uri, function( err, data ) {
-      console.log( uri );
+
+      //returns error file if path is invalid
       if ( err ) {
         return fs.readFile( './public/404.html', function( err, data ) {
           respond.writeHead( 404, {
@@ -24,6 +26,8 @@ module.exports = (function() {
           respond.end( data );
         });
       }
+
+      //loads file with proper path request
       respond.writeHead( 200, {
         'Server' : 'Rizzi-lush',
         'Content-length' : data.length
