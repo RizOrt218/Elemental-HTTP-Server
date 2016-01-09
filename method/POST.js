1ca065c5fad865342a736b0e195c380733864da4
RizOrt218 <rizhiafalen@gmail.com>
2016-01-08 23:00:30 -1000

    exported POST into a diff file, still need to do more refactoring

--- method/POST.js
+++ method/POST.js
@@ -4,21 +4,22 @@ const fs          = require('fs');
 const path        = require('path');
 const querystring = require( 'querystring' );
 const url         = require( 'url' );
-const tempHandler = request( './templateFile/tempHandler' );
+// const tempHandler = request( './templateFile/tempHandler' );
 
 module.exports = (function() {
+  var newFileName;
+  var elemCount = 2;
 
   var POST = function( request, respond ) {
-    let uri = request.url;
+    var uri = request.url;
 
     //lets find out what the client is posting
     request.on( 'data', function( buffer ) {
       var dataBuffer = querystring.parse( buffer.toString() );
 
       if ( uri === '/elements' ) {
-        tempHandler( request, respond );
 
-        return fs.readFile( 'elementalTemplate.html' , function ( err , template ) {
+        return fs.readFile( './elementalTemplate.html' , function ( err , template ) {
           newFileName = dataBuffer.elementName;
 
           var tempString = template.toString()
@@ -29,14 +30,14 @@ module.exports = (function() {
 
           return fs.writeFile( './public/' + newFileName  + '.html', tempString , function( err, tempString ) {
 
-            socket.writeHead( 200, {
+            respond.writeHead( 200, {
               'Server' : 'Rizzi-lush',
             });
-            socket.end( tempString );
+            // respond.end( tempString );
 
-            return fs.readFile( 'indexTemplate.html', function ( err, contents ) {
+            return fs.readFile( './indexTemplate.html', function ( err, contents ) {
               var pageLink = '<li><a href="/{{elementName}}.html">{{elementName}}</a></li>';
-              elemCount += 1;
+                elemCount += 1;
 
               var appendNewElem = contents.toString()
                 .replace( '<!--  {{ element list }} -->', pageLink + '<!--  {{ element list }} -->' )
@@ -44,16 +45,15 @@ module.exports = (function() {
                 .replace( /\d+<!-- number -->/, elemCount + '<!-- number -->');
 
               return fs.writeFile( './public/index.html', appendNewElem , function( err ) {
-                fs.writeFile( 'indexTemplate.html', appendNewElem , function( err ) {
-
-                  console.log(tempString);
-                  // addElemInArr();
-
+                fs.writeFile( './indexTemplate.html', appendNewElem , function( err ) {
+                  if ( err ) {
+                    console.log ( 'error' );
+                  }
                 });
-                socket.writeHead( 200, {
+                respond.writeHead( 200, {
                   'Server' : 'Rizzi-lush',
                  });
-                socket.end( );
+                respond.end( );
               });
             }); // end of return fs.readFile( 'index..
           }); // end fs.writeFile
