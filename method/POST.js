cd7de3c39109cba2ae8c539e4066f48931598c7e
RizOrt218 <rizhiafalen@gmail.com>
2016-01-08 23:38:21 -1000

    more refactoring, cleaning up and adds comments

--- method/POST.js
+++ method/POST.js
@@ -4,62 +4,53 @@ const fs          = require('fs');
 const path        = require('path');
 const querystring = require( 'querystring' );
 const url         = require( 'url' );
-// const tempHandler = request( './templateFile/tempHandler' );
+const updateIndex = require( '../templateFile/indexHandler' );
 
 module.exports = (function() {
   var newFileName;
   var elemCount = 2;
 
-  var POST = function( request, respond ) {
+  var POST = function ( request, respond ) {
     var uri = request.url;
 
     //lets find out what the client is posting
-    request.on( 'data', function( buffer ) {
+    request.on( 'data', function ( buffer ) {
       var dataBuffer = querystring.parse( buffer.toString() );
 
+      //if client is on this uri
       if ( uri === '/elements' ) {
 
+        //when client creates a post, this will generate a new file with client's new post
         return fs.readFile( './elementalTemplate.html' , function ( err , template ) {
+
+          //getting the clients' new file name input
           newFileName = dataBuffer.elementName;
 
-          var tempString = template.toString()
+          if ( err ) { //do later
+            console.log( 'error' );
+          }
+
+          //will replace template elements with new values
+          var renderTemplate = template.toString()
             .replace( /{{elementName}}/g, dataBuffer.elementName )
             .replace( '{{elementSymbol}}', dataBuffer.elementSymbol )
             .replace( '{{elementAtomicNumber}}', dataBuffer.elementAtomicNumber )
             .replace( '{{elementDescription}}', dataBuffer.elementDescription );
 
-          return fs.writeFile( './public/' + newFileName  + '.html', tempString , function( err, tempString ) {
+          //create brand new file with client's post
+          return fs.writeFile( './public/' + newFileName + '.html', renderTemplate, function ( err ) {
 
-            respond.writeHead( 200, {
+            respond.writeHead( 200, { //do later
               'Server' : 'Rizzi-lush',
             });
-            // respond.end( tempString );
-
-            return fs.readFile( './indexTemplate.html', function ( err, contents ) {
-              var pageLink = '<li><a href="/{{elementName}}.html">{{elementName}}</a></li>';
-                elemCount += 1;
-
-              var appendNewElem = contents.toString()
-                .replace( '<!--  {{ element list }} -->', pageLink + '<!--  {{ element list }} -->' )
-                .replace( /{{elementName}}/g, dataBuffer.elementName )
-                .replace( /\d+<!-- number -->/, elemCount + '<!-- number -->');
-
-              return fs.writeFile( './public/index.html', appendNewElem , function( err ) {
-                fs.writeFile( './indexTemplate.html', appendNewElem , function( err ) {
-                  if ( err ) {
-                    console.log ( 'error' );
-                  }
-                });
-                respond.writeHead( 200, {
-                  'Server' : 'Rizzi-lush',
-                 });
-                respond.end( );
-              });
-            }); // end of return fs.readFile( 'index..
+            // respond.end( renderTemplate );
           }); // end fs.writeFile
         }); //end of fs.readFile
       } // end of if ( uri === '/elements' )
-});
+    });
+
+    //invoke to update index file
+    updateIndex(request, respond);
 
   }; // end of POST function
 
---
+++ templateFile/indexHandler.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const fs          = require('fs');
+const path        = require('path');
+const querystring = require( 'querystring' );
+
+module.exports = (function() {
+  var elemCount = 2;
+
+  var updateIndex = function( request, respond ) {
+
+    request.on( 'data', function ( buffer ) {
+      var dataBuffer = querystring.parse( buffer.toString() );
+
+      //** this will update the index with new element and element count
+      return fs.readFile( './indexTemplate.html', function ( err, contents ) {
+        var pageLink = '<li><a href="/{{elementName}}.html">{{elementName}}</a></li>';
+          elemCount += 1;
+
+        var appendNewElem = contents.toString()
+          .replace( '<!--  {{ element list }} -->', pageLink + '<!--  {{ element list }} -->' )
+          .replace( /{{elementName}}/g, dataBuffer.elementName )
+          .replace( /\d+<!-- number -->/, elemCount + '<!-- number -->');
+
+        return fs.writeFile( './public/index.html', appendNewElem , function( err ) {
+          fs.writeFile( './indexTemplate.html', appendNewElem , function( err ) {
+            if ( err ) { //do later
+              console.log ( 'error' );
+            }
+          });
+          respond.writeHead( 200, {
+            'Server' : 'Rizzi-lush',
+           });
+          respond.end( );
+        });
+      }); // end of return fs.readFile(
+    });
+
+  };
+  return updateIndex;
+}());
